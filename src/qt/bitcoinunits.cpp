e32c7710bdbae315cdacead41a2aaf052e543aaf
diMarcoDEV <dimarco@hbgrp.nl>
2018-04-24 14:07:57 +0200

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("OTWT");
+    case mBTC: return QString("Milli-OTWT (1 / 1,000)");
+    case uBTC: return QString("Micro-OTWT (1 / 1,000,000)");
     default: return QString("???");
     }
 }
