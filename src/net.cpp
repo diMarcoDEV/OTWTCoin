0137a7318e55cee99c5ae913c4242026c603a755
diMarcoDEV <diMarco@hbgrp.nl>
2018-04-25 21:31:03 +0200

    v1.0.0

--- src/net.cpp
+++ src/net.cpp
@@ -362,7 +362,7 @@ bool GetMyExternalIP(CNetAddr& ipRet)
         //  <?php echo $_SERVER["REMOTE_ADDR"]; ?>
         if (nHost == 1)
         {
-            addrConnect = CService("91.198.22.70",80); // checkip.dyndns.org
+            addrConnect = CService("216.146.43.70",80); // checkip.dyndns.org
 
             if (nLookup == 1)
             {
